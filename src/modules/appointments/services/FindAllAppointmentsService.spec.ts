bcd690d37a200b8b59a1387edc0fbba946665d03
Fernando Lisboa <fernandoigorlisboa@gmail.com>
2020-07-20 20:49:47 -0300

    should be able get all appointments

---
+++ src/modules/appointments/services/FindAllAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+
+import CreateAppointmentService from './CreateAppointmentService'
+import FindAllAppointmentsService from './FindAllAppointmentsService'
+
+describe('FindAllAppointments', () => {
+    it('should be able get all appointments', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+
+        const createAppointmentService = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        )
+
+        const provider_id = 'random-provider-id'
+
+        await createAppointmentService.execute({
+            provider_id,
+            date: new Date(2020, 6, 10, 12),
+        })
+
+        await createAppointmentService.execute({
+            provider_id,
+            date: new Date(2020, 6, 10, 13),
+        })
+
+        await createAppointmentService.execute({
+            provider_id,
+            date: new Date(2020, 6, 10, 14),
+        })
+
+        const findAllAppointmentsService = new FindAllAppointmentsService(
+            fakeAppointmentsRepository,
+        )
+
+        const appointments = await findAllAppointmentsService.execute()
+
+        expect(appointments).toHaveLength(3)
+    })
+})
