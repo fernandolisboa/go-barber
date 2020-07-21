c394ef763442f1d9916a1a8456449f64e4a4e125
Fernando Lisboa <fernandoigorlisboa@gmail.com>
2020-07-21 12:54:55 -0300

    tests for service CreateUser

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,54 @@
+import AppError from '@shared/errors/AppError'
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository'
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider'
+
+import CreateUserService from './CreateUserService'
+
+describe('CreateUser', () => {
+    it('should be able to create a new user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const fakeHashProvider = new FakeHashProvider()
+
+        const createUserService = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        )
+
+        const { name, email, password } = {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            password: '123456',
+        }
+
+        const user = await createUserService.execute({ name, email, password })
+
+        expect(user).toHaveProperty('id')
+        expect(user.name).toBe(name)
+        expect(user.email).toBe(email)
+    })
+
+    it('should not be able to create two users with the same e-mail', async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const fakeHashProvider = new FakeHashProvider()
+
+        const createUserService = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        )
+
+        await createUserService.execute({
+            name: 'John Doe 1',
+            email: 'john@doe.com',
+            password: '123456-1',
+        })
+
+        const promise = createUserService.execute({
+            name: 'John Doe 2',
+            email: 'john@doe.com',
+            password: '123456-2',
+        })
+
+        expect(promise).rejects.toBeInstanceOf(AppError)
+    })
+})
