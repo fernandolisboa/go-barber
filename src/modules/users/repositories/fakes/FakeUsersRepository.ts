5c3161edf79b9768b1ea7cc55f6e972dfc2b8a49
Fernando Lisboa <fernandoigorlisboa@gmail.com>
2020-07-21 12:53:37 -0300

    created fake users repository

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,47 @@
+import { uuid } from 'uuidv4'
+
+import IUsersRepository from 'modules/users/repositories/IUsersRepository'
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
+
+import User from '@modules/users/infra/typeorm/entities/User'
+
+class FakeUsersRepository implements IUsersRepository {
+    private users: User[] = []
+
+    public async findAll(): Promise<User[]> {
+        return this.users
+    }
+
+    public async findById(id: string): Promise<User | undefined> {
+        const userFound = this.users.find(user => user.id === id)
+        return userFound
+    }
+
+    public async findByLogin(login: string): Promise<User | undefined> {
+        const userFound = this.users.find(
+            ({ email, username }) => email === login || username === login,
+        )
+
+        return userFound
+    }
+
+    public async create(userData: ICreateUserDTO): Promise<User> {
+        const user = new User()
+
+        Object.assign(user, { id: uuid() }, userData)
+
+        this.users.push(user)
+
+        return user
+    }
+
+    public async save(user: User): Promise<User> {
+        const userIndex = this.users.findIndex(({ id }) => id === user.id)
+
+        this.users[userIndex] = user
+
+        return user
+    }
+}
+
+export default FakeUsersRepository
