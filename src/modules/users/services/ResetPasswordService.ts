e53255c7f3a79a0ee8083989783a25cdd0a738a0
Fernando Lisboa <fernandoigorlisboa@gmail.com>
2020-07-29 18:42:37 -0300

    fixed import

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -5,7 +5,7 @@ import AppError from '@shared/errors/AppError'
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository'
-import IHashProvider from '../providers/HashProvider/models/IHashProvider'
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider'
 
 interface IRequest {
   token: string
