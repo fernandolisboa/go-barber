6c38289d16bb04c8835a1318cede44f996861364
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-02 19:06:57 -0300

    added customer_id to ICreateAppointmentDTO

--- src/modules/appointments/dtos/ICreateAppointmentDTO.ts
+++ src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -1,5 +1,8 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 
-type ICreateAppointmentDTO = Pick<Appointment, 'provider_id' | 'date'>
+type ICreateAppointmentDTO = Pick<
+  Appointment,
+  'provider_id' | 'customer_id' | 'date'
+>
 
 export default ICreateAppointmentDTO
