0a38c2c0db68fbbf30e26eff2382e89de8c6e029
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-04 16:40:47 -0300

    app.use(celebrate errors)

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,8 @@ import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 
+import { errors } from 'celebrate'
+
 import uploadConfig from '@config/upload'
 import AppError from '@shared/errors/AppError'
 
@@ -18,6 +20,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
+
+app.use(errors())
 
 app.use(
   (err: Error, _req: Request, response: Response, _next: NextFunction) => {
