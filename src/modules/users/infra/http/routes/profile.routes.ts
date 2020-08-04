6e49f51dc07d422964e51b09b286df5b43616497
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-04 17:11:18 -0300

    added celebrate to profile routes

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
@@ -11,6 +12,27 @@ const profileRouter = Router()
 profileRouter.use(ensureAuthenticated)
 
 profileRouter.get('/', profileController.show)
-profileRouter.put('/', profileController.update)
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string().when('new_password', {
+        is: Joi.string().min(1),
+        then: Joi.required(),
+        otherwise: Joi.optional(),
+      }),
+      new_password: Joi.string(),
+      password_confirmation: Joi.string().when('new_password', {
+        is: Joi.string().min(1),
+        then: Joi.required().valid(Joi.ref('new_password')),
+        otherwise: Joi.optional(),
+      }),
+    },
+  }),
+  profileController.update,
+)
 
 export default profileRouter
