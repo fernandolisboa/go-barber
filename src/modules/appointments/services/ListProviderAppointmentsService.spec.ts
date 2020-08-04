ccf054e61d266e979a12e42fa5bad6b08c97903b
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-04 03:43:42 -0300

    implementation, tests, controller & route for listing appointments of logged provider in a given date (day, month & year)

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,70 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository'
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService'
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository
+
+let listProviderAppointments: ListProviderAppointmentsService
+
+const provider_id = 'any-provider-id'
+const customer_id = 'any-customer-id'
+
+const day = 22
+const month = 4
+const year = 2020
+
+let appointment1: Appointment
+let appointment2: Appointment
+let appointment3: Appointment
+let appointment4: Appointment
+
+describe('ListProviderAppointments', () => {
+  beforeEach(async () => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    )
+
+    appointment1 = await fakeAppointmentsRepository.create({
+      provider_id,
+      customer_id,
+      date: new Date(2020, 3, 22, 8, 0, 0),
+    })
+
+    appointment2 = await fakeAppointmentsRepository.create({
+      provider_id,
+      customer_id,
+      date: new Date(2020, 3, 22, 9, 0, 0),
+    })
+
+    appointment3 = await fakeAppointmentsRepository.create({
+      provider_id,
+      customer_id,
+      date: new Date(2020, 3, 22, 15, 0, 0),
+    })
+
+    appointment4 = await fakeAppointmentsRepository.create({
+      provider_id,
+      customer_id,
+      date: new Date(2020, 3, 22, 17, 0, 0),
+    })
+  })
+
+  it('should be able to list the provider appointments of a specific day', async () => {
+    const availability = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    })
+
+    expect(availability).toEqual([
+      appointment1,
+      appointment2,
+      appointment3,
+      appointment4,
+    ])
+  })
+})
