a2cfd7481ea6c4861ae7b034d75b85fce99b28ed
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-08-05 22:44:44 -0300

    invalidate all cache prefixed with given key

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,6 +2,7 @@ interface ICacheProvider {
   save<T>(key: string, value: T): Promise<void>
   get<T>(key: string): Promise<T | null>
   invalidate(key: string): Promise<void>
+  invalidatePrefix(prefix: string): Promise<void>
 }
 
 export default ICacheProvider
